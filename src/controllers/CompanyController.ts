f6ca633ce8698944b289e0433661a301f899eb9f
Filipe Figueiredo <filipefig26@gmail.com>
2024-12-06 17:36:40 +0000

    removing dead code and adding a log to this exception

--- src/controllers/CompanyController.ts
+++ src/controllers/CompanyController.ts
@@ -11,6 +11,7 @@ import {BadRequestError} from '@/types/errors';
 import {CreateCompanyForm} from "@/types/api"
 import {Company} from "@prisma/client";
 import {CompanyService} from "@/services/CompanyService";
+import logger from "@/utils/logger";
 
 const MISSING_NAME_COMPANY_ERR = 'Company name is required';
 
@@ -34,36 +35,12 @@ export class CompanyController {
         if (!name) {
             throw new BadRequestError(MISSING_NAME_COMPANY_ERR);
         }
-        // const existingCompany = await CompanyService.findByName(name);
-        // if (existingCompany) {
-        //     throw new BadRequestError(EXISTING_COMPANY_ERR);
-        // }
         try {
             return CompanyService.create(createConfigBody);
         } catch (e) {
-            console.error(e);
+            logger.error(e);
             return CompanyService.findByName(createConfigBody.name);
         }
     }
-    // public async createCompany(@Body() createConfigBody: CreateCompanyForm): Promise<Company> {
-    //     return new Promise(async (resolve, reject) => {
-    //         const {name} = createConfigBody;
-    //         if (!name)
-    //             return reject(new BadRequestError(MISSING_NAME_COMPANY_ERR));
-    //         try {
-    //             if (await CompanyService.findByName(name) !== null)
-    //                 return reject(new BadRequestError(EXISTING_COMPANY_ERR));
-    //         } catch (e) {
-    //             console.error(e);
-    //         }
-    //         try {
-    //             return resolve(await CompanyService.create(createConfigBody) as Company);
-    //         } catch (e) {
-    //             if (e instanceof ExistingError) {
-    //                 return reject(new BadRequestError(EXISTING_COMPANY_ERR));
-    //             }
-    //         }
-    //     })
-    // }
 }
 
